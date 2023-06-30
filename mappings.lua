f582e6dfebf7f29e50e3be4da95429caa0095863
MSBarbieri <matheussouzabarbieri@gmail.com>
2023-06-30 10:56:28 -0300

    add base configs for astronvim

--- mappings.lua
+++ mappings.lua
@@ -3,24 +3,144 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
+local code = {
+  ["<leader>."] = { name = "code" },
+}
+
+local find = {
+  ["<leader>fe"] = { "<cmd>Telescope live_grep<cr>", desc = "find words" },
+  ["<leader>f,"] = { "<cmd>TodoTelescope<cr>", desc = "Find Todos" },
+  ["<leader>f."] = { "<cmd>Telescope diagnostics<cr>", desc = "Find Diagnostics" },
+}
+local fix = {
+  ["<leader>,"] = { name = "fix" },
+  ["<leader>,t"] = { "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
+  ["<leader>,c"] = { vim.lsp.buf.code_action, desc = "Quickfix" },
+
+}
+local navigation = {
+  ["<leader>h"] = { name = "Harpoon" },
+  ["<leader>ha"] = { "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "add harpoon file" },
+  ["<leader>ht"] = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "harpoon ui" },
+  ["<leader>hh"] = { "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Jump to previous" },
+  ["<leader>hl"] = { "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Jump to next" },
+  ["<leader>h+"] = { "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Jump to 1" },
+  ["<leader>h["] = { "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Jump to 2" },
+  ["<leader>h{"] = { "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Jump to 3" },
+  ["<leader>h("] = { "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "Jump to 4" },
+  ["<leader>h#"] = { "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", desc = "Jump to 5" },
+  ["<leader>h="] = { "<cmd>lua require('harpoon.ui').nav_file(6)<cr>", desc = "Jump to 6" },
+  ["<leader>h)"] = { "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", desc = "Jump to 7" },
+  ["<leader>h}"] = { "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", desc = "Jump to 8" },
+  ["<leader>h]"] = { "<cmd>lua require('harpoon.ui').nav_file(9)<cr>", desc = "Jump to 9" },
+  ["<leader>h*"] = { "<cmd>lua require('harpoon.ui').nav_file(10)<cr>", desc = "Jump to 10" },
+  s = {
+    function()
+      require("hop").hint_char2({
+        direction = require 'hop.hint'.HintDirection.AFTER_CURSOR,
+        case_inssensitive = true,
+      })
+    end,
+    desc = "hop"
+  },
+  f = {
+    function()
+      require("hop").hint_char1({
+        direction = require 'hop.hint'.HintDirection.AFTER_CURSOR,
+        current_line_only = true,
+        case_inssensitive = true,
+        hint_offset = 1
+      })
+    end,
+    desc = "hop"
+  },
+  t = {
+    function()
+      require("hop").hint_char1({
+        direction = require 'hop.hint'.HintDirection.AFTER_CURSOR,
+        current_line_only = true,
+        case_inssensitive = true,
+        hint_offset = -1
+      })
+    end,
+    desc = "hop"
+  },
+  S = {
+    function()
+      require("hop").hint_char2({
+        direction = require 'hop.hint'.HintDirection.BEFOR_CURSOR,
+        case_inssensitive = true,
+      })
+    end,
+    desc = "hop"
+  },
+  F = {
+    function()
+      require("hop").hint_char2({
+        direction = require 'hop.hint'.HintDirection.BEFOR_CURSOR,
+        current_line_only = true,
+        case_inssensitive = true,
+        hint_offset = 1
+      })
+    end,
+    desc = "hop"
+  },
+  T = {
+    function()
+      require("hop").hint_char1({
+        direction = require 'hop.hint'.HintDirection.BEFOR_CURSOR,
+        current_line_only = true,
+        case_inssensitive = true,
+        hint_offset = -1
+      })
+    end,
+    desc = "hop"
+  },
+}
+
+local fold = {
+  ["<leader>m"] = { name = "fold" },
+  ["<leader>mc"] = { require('fold-cycle').close_all, desc = "Close" },
+  ["<leader>mt"] = { require('fold-cycle').toggle_all, desc = "Toggle" }
+}
+
+local function assign(tbl, props)
+  for k, v in pairs(props) do
+    tbl[k] = v
+  end
+  return tbl
+end
+
+local normal = function()
+  local tbl = assign(code, find)
+  tbl = assign(tbl, fix)
+  tbl = assign(tbl, navigation)
+  tbl = assign(tbl, fold)
+  return tbl
+end
+
 return {
   -- first key is the mode
-  n = {
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "Buffer"
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
-      end,
-      desc = "Pick to close",
-    },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-  },
+  n = normal(),
+
+  -- second key is the lefthand side of the map
+  -- mappings seen under group name "Buffer"
+  -- ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+  -- ["<leader>bD"] = {
+  --   function()
+  --     require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+  --       require("astronvim.utils.buffer").close(
+  --         bufnr)
+  --     end)
+  --   end,
+  --   desc = "Pick to close",
+  -- },
+
+  -- tables with the `name` key will be registered with which-key if it's installed
+  -- this is useful for naming menus
+  -- ["<leader>b"] = { name = "Buffers" },
+  -- quick save
+  -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,117 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+	-- You can also add new plugins here as well:
+	"RishabhRD/nvim-cheat.sh",   --
+	"danilamihailov/beacon.nvim", -- cursor window movement blink
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {}
+	},
+	"nvim-treesitter/nvim-treesitter-context", -- function context on top
+	{
+		'nacro90/numb.nvim',
+		config = function()
+			require('numb').setup()
+		end
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup()
+		end
+	},
+	{
+		'jghauser/fold-cycle.nvim',
+		config = function()
+			require('fold-cycle').setup({
+				open_if_max_closed = true, -- closing a fully closed fold will open it
+				close_if_max_opened = true, -- opening a fully open fold will close it
+			})
+		end
+	},
+	{
+		"zbirenbaum/copilot.lua",
+		event = { "VimEnter" },
+		config = function()
+			vim.defer_fn(function()
+				require("copilot").setup {
+					suggestion = {
+						enable = true,
+						auto_trigger = true,
+						keymap = {
+							next = "<C-h>",
+							prev = "<C-l>",
+							accept = "<C-c>"
+						},
+					}
+				}
+			end, 100)
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		after = { "copilot.lua", "nvim-cmp" },
+		config = function()
+			require("copilot_cmp").setup()
+		end
+	},
+	{
+		"phaazon/hop.nvim",
+		event = "BufRead",
+		config = function()
+			require('hop').setup({
+				multi_windows = false,
+				case_insensitive = true,
+				teasing = true,
+				jump_on_sole_occurrence = true,
+				keys = "aoeuhtns;,.pgcrl'qjkmwvzd",
+			})
+		end,
+	},
+	{
+		"ThePrimeagen/harpoon",
+		config = function()
+			require('harpoon').setup({
+				menu = {
+					width = vim.api.nvim_win_get_width(0) - 4,
+					height = 8
+				}
+			})
+		end
+	},
+	{
+		"windwp/nvim-spectre",
+		config = function() -- Super Search
+			require("spectre").setup({
+				mapping = {
+					['send_to_qf'] = {
+						map = "<leader>e",
+						cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+						desc = "send all item to quickfix"
+					},
+				}
+			})
+		end
+	},
+	{ 'kevinhwang91/nvim-bqf' }, -- BETTER quickfix
+	{
+		"kylechui/nvim-surround", --  the YSSR
+		version = "*",            -- Use for stability; omit to use `main` branch for the latest features
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end
+	},
+	{
+		"folke/tokyonight.nvim",
+		config = function()
+			require("tokyonight").setup({
+				transparent = true,
+				comments = { bold = true, italic = false },
+				keywords = { bold = true, italic = false },
+			})
+		end
+	},
 }
